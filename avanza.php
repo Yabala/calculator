0e616f56dd22cb74fe8e6459d043f97ef15adc66
Yabala <jfager@cse.edu.uy>
2015-11-03 18:17:36 -0300

    Actualización

---
+++ avanza.php
@@ -0,0 +1,62 @@
+
+
+
+<html>
+<head>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+
+
+
+
+
+
+
+<!-- ------------------------------------------------------------------------------------------------------------------>
+<!-- INICIO ENCABEZADO ------------------------------------------------------------------------------------------------>
+<!-- ------------------------------------------------------------------------------------------------------------------>
+
+<h1><i class="fa fa-calculator"></i> CALCULATOR++</h1>
+<h2>Untitle - Filckr</h2>
+
+
+
+
+
+
+<!-- ------------------------------------------------------------------------------------------------------------------>
+<!-- INICIO FORMULARIO ADMINISTRAR MATERIALES ------------------------------------------------------------------------------>
+<!-- ------------------------------------------------------------------------------------------------------------------>
+
+<form name='ocDel' method='post' action='del.php' class='admin'>
+<div class='admin'>Metadata</div><br /><input name='nombre' value='ejemplo' type='hidden' />
+<table class='admin'><tr class='admin'><td class='admin'>T&iacute;tulo</td><td class='admin'>Formato</td><td class='admin'>Keyword</td><td class='admin'>Autor</td><td class='admin'>URL</td><td class='admin'>Licencia</td></tr><tr class='admin'><td class='admin'>Untitle</td><td class='admin'>IMAGE</td><td class='admin'>Leica Summicron-R 35mm f/2 E55</td><td class='admin'>&#25885;&#21040;&#25499;</td><td class='admin'>https://www.flickr.com/photos/md903152/19421411035/in/photostream/</td><td class='admin'>CC0</td></tr></table><br />
+</form>
+<!-- ------------------------------------------------------------------------------------------------------------------>
+<!-- FIN FORMULARIO ADMINISTRAR MATERIALES --------------------------------------------------------------------------------->
+<!-- ------------------------------------------------------------------------------------------------------------------>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ ejemplo.php
@@ -0,0 +1,50 @@
+<?php
+
+
+
+// This file is part of Calculator https://github.com/Yabala/calculator
+//
+// Calculator is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Calculator is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Calculator.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+?>
+
+
+
+<html>
+<head>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<h1><i class="fa fa-calculator"></i> CALCULATOR++</h1>
+
+
+<form name="createOP" method="post" action="avanza.php" class="index">
+
+<div class="index">
+Escriba la URL del recurso.
+</div>
+<p>
+URL: <input type="text" name="nombre" size="100" value="">
+</p>
+<input value="INTENTAR OBTENER METADATA" type="submit"  id='submit' />
+
+</form>
+
+</body>
+</html>
